c6dee309fbdae21c562ba0f961aec20d84a1719f
blakerutledge <blake@blakerutledge.com>
2015-11-14 16:46:30 -0500

    starting project

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+import Pt from "./libs/pt.js";
+
+class Main {
+  constructor(options = {}) {
+  	console.log("this");
+
+
+
+
+  }
+}
+
+document.addEventListener('DOMContentLoaded', event => new Main)
