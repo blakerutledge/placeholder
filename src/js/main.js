fb1df4ebe4d52ba5443cc1727532c6b6db639d17
blakerutledge <blake@blakerutledge.com>
2015-11-15 15:56:03 -0500

    copy to clipboard, mobile animation, responsive layout, its a wrap.

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,15 @@
 //import Pt from "./libs/pt.js";
 import verge from "./libs/verge.js";
+import Clipboard from "./libs/clipboard.min.js";
 
-
-var space, form, center, viewPort, bot, lastTime, colorIndex, mouseSpeed
+var space, form, center, viewPort, bot, lastTime, colorIndex, mouseSpeed, timeout
 
 var pt = document.querySelectorAll("#pt")
 
-lastTime = 0
+lastTime = -5000
 colorIndex = 0
+mouseSpeed = 4000
+
 
 class Main {
 	constructor(options = {}) {
@@ -18,50 +20,83 @@ class Main {
 		});
 		window.addEventListener('mousemove', function(event){
 		  mouseSpeed = getMouseSpeed(event);
+		  event.stopPropagation();
+		  clearTimeout(timeout);
+  		  timeout = setTimeout(function(){mouseSpeed = 4000;}, 200);
 		});
+		window.addEventListener("touchstart", function(event){
+		  clickAdd(event);
+		});
+		window.addEventListener("mousedown", function(event){
+		  clickAdd(event);
+		});
+
+		var clipboard = new Clipboard('#footer-email');
+		
+		clipboard.on('success', function(e) {
+			document.querySelector(".copied-alert-triangle").classList.add("copied-alert-showing");
+			document.querySelector(".copied-alert").classList.add("copied-alert-showing");
+			setTimeout(function(){
+				document.querySelector(".copied-alert-triangle").classList.remove("copied-alert-showing");
+				document.querySelector(".copied-alert").classList.remove("copied-alert-showing");
+			}, 3000);
+		    
+		});
+
+		clipboard.on('error', function(e) {
+			document.querySelector(".copied-alert-triangle").classList.add("copied-alert-showing");
+			document.querySelector(".copied-alert").classList.add("copied-alert-showing");
+			document.querySelector(".copied-alert").innerHTML = "Error copying.";
+			setTimeout(function(){
+				document.querySelector(".copied-alert-triangle").classList.remove("copied-alert-showing");
+				document.querySelector(".copied-alert").classList.remove("copied-alert-showing");
+				e.clearSelection();
+			}, 3000);
+		});
+    	
   }
 }
+
+// A Shape is a kind of Vector
+function Shape() {
+  Vector.apply( this, arguments ); // call Vector's constructor
+  this.age = 0
+  this.maxAge = 450
+  this.size =  0
+  this.color = "#00FFFF";
+}
+Util.extend( Shape, Vector ); // extends Vector class
+
+// define an animate function so it can be animated when added into Space
+Shape.prototype.animate = function(time, fps, context) {
+  if (this.age++ > this.maxAge) space.remove(this)
+
+  // draw shapes
+  var progress = this.age / this.maxAge
+  var opacity = progress > 0.7 ? 1 - ( progress - 0.7 ) / 0.2  : 1
+  form.fill( Util.toRGBColor( this.color, true, opacity ) )
+  var largerSize = space.size.x > space.size.x ? space.size.x : space.size.y
+  this.size = Math.pow( progress, 1/2 ) * largerSize
+  form.point( this, this.size, true )
+
+}
+
+var colors = ['#27AAE1', '#F57558', '#00C29E', '#82626D']
 
 function buildPt() {
 
 	//// 1. Define Space and Form
-	var colors = ["#ff7b21", "#3dc895", "#ffd138", "#916e79"]
-
 	space = new CanvasSpace("demo", "#fff" ).display();
-
 	form = new Form( space );
 	form.stroke( false );
 
+	center = space.size.$divide(2)
+	mousex = center.x
+	mousey = center.y
 
 	//// 2. Create Elements
-
-	// A Shape is a kind of Vector
-	function Shape() {
-	  Vector.apply( this, arguments ); // call Vector's constructor
-	  this.age = 0
-	  this.maxAge = 200
-	  this.size =  0
-	  this.color = "#00FFFF";
-	}
-	Util.extend( Shape, Vector ); // extends Vector class
-
-	// define an animate function so it can be animated when added into Space
-	Shape.prototype.animate = function(time, fps, context) {
-	  if (this.age++ > this.maxAge) space.remove(this)
-
-	  // draw shapes
-	  var progress = this.age / this.maxAge
-	  var opacity = progress > 0.7 ? 1 - ( progress - 0.7 ) / 0.2  : 1
-	  form.fill( Util.toRGBColor( this.color, true, opacity ) )
-	  var largerSize = space.size.x > space.size.x ? space.size.x : space.size.y
-	  this.size = Math.pow( progress, 1/2 ) * largerSize
-	  form.point( this, this.size, true )
-
-	}
-
     bot = {
         animate: function( time, fs, context ) {
-	  	  // add two Dust into space
 		  if ( ( time - lastTime ) > mouseSpeed ) {
 				var me = new Shape( mousex, mousey )
 				me.color = colors[colorIndex]
@@ -72,8 +107,7 @@ function buildPt() {
         }
     }
 
-	center = space.size.$divide(2)
-	console.log(center.x)
+	
 
 	// 4. Start playing
 	space.add(bot);
@@ -88,6 +122,9 @@ function updatePt() {
 	}
 
 	center = space.size.$divide(2)
+
+	mousex = center.x
+	mousey = center.y
 
 }
 
@@ -104,15 +141,35 @@ function getMouseSpeed(e) {
 	if (lastmousex > -1) {
 		mousetravel = Math.max( Math.abs(mousex-lastmousex), Math.abs(mousey-lastmousey) );
 	}
-	lastmousex = mousex;
-	lastmousey = mousey;
 
-	mousetravel = mousetravel>100 ? 100 : mousetravel*5
-	mousetravel = ( 100 - mousetravel ) / 100
+	if (mousex===lastmousex && mousey===lastmousey) {
+		//didnt move, stop firing more shit
+		mouseSpeed = 4000;
+		return mouseSpeed;
+	}
+	else {
+		lastmousex = mousex;
+		lastmousey = mousey;
 
-	mouseSpeed = mousetravel*4000
-	mouseSpeed = mouseSpeed<50 ? 50 : mouseSpeed
-	return mouseSpeed;
+		mousetravel = mousetravel>100 ? 100 : mousetravel*5
+		mousetravel = ( 100 - mousetravel ) / 100
+
+		mouseSpeed = mousetravel*4000
+		mouseSpeed = mouseSpeed<50 ? 50 : mouseSpeed
+	
+		return mouseSpeed;
+	}
+}
+
+function clickAdd(e) {
+	mousex = (e.touches!=undefined) ? e.touches[0].pageX : e.pageX;
+	mousey = (e.touches!=undefined) ? e.touches[0].pageY : e.pageY;
+
+	var me = new Shape( mousex, mousey )
+	me.color = colors[colorIndex]
+	space.add( me )
+	colorIndex = colorIndex < 3 ? colorIndex+1 : 0;
+
 }
 
 document.addEventListener('DOMContentLoaded', event => new Main)
